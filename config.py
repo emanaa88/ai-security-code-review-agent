873a2ea2e1a2adee07f739cf653cae684c76d287
emanaa88 <143397483+emanaa88@users.noreply.github.com>
2026-07-08 14:58:52 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 API_KEY = "sk-live-4f9a8b2c1d3e5f7890abcdef1234567890"
-db_query = "SELECT * FROM users WHERE id = " + user_input
+   
+   def get_user(user_input):
+       db_query = "SELECT * FROM users WHERE id = " + user_input
+       return db_query
